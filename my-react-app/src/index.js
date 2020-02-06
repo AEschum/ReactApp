17418c68b3f661014e11f4305c38da0308f64d90
AEschum <andreweschum@gmail.com>
2020-02-06 11:24:50 -0600

    Okta Authentication Added

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const oktaConfig = {
+    issuer: `https://dev-898892.okta.com/oauth2/default`,
+    redirect_uri: `${window.location.origin}/implicit/callback`,
+    client_id: `0oa23ygn2QuPczezY4x6`,
+};
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Security {...oktaConfig}>
+            <App />
+        </Security>
+    </BrowserRouter>, 
+    document.getElementById('root'),);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
